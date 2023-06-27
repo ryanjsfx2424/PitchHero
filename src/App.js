886b5bdbb961c9bcd9a7203385dea73954743e45
Ryan Farber <ryanjsfx@MB-145.fritz.box>
2023-06-27 17:04:40 +0200

    updating the mint function to select available nfts from the trait categories

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Homepage from "./Homepage";
 import Mint from "./MintV3";
 
 function App() {
-	const url = window.location.href;
-	const isMintPage = url.includes("mint");
 	return (
-		<>
-			{
-				(isMintPage) ?
-					<Mint /> 
-				: 
-					<Homepage />
-			}
-		</>
+					<Mint />
 	)
-		{/* <Switch>
-			<Route path="/" element={<Homepage />} />
-			<Route path="/mint" element={<Mint />} />
-		</Switch> */}
 }
 
 export default App;
